5c053f13f0864f262a44f9e5071e80cd1e67026b
Kieran-Go <187461911+Kieran-Go@users.noreply.github.com>
2025-03-26 13:44:26 +1030

    Add CSS styles to ensure pleasing user experience

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -1,15 +1,18 @@
+import "../css/Heading.css"
+
 // Returns the HTML heading section which includes a header, the scoreboard, and a brief explanation of the game rules
 function Heading({score, highScore}){
     return(
         <section className="heading">
-            <h1>Pokémon Memory Game</h1>
-
-            <div className="scoreboard">
-                <p>Score: {score}</p>
-                <p>High Score: {highScore}</p>
+            <div className="heading-info">
+                <h1>Pokémon Memory Game</h1>
+                <p>Get points by clicking on an image. But don't click on any more than once!</p>
             </div>
 
-            <p>Get points by clicking on an image. But don't click on any more than once!</p>
+            <div className="scoreboard">
+            <p>Score: {score}</p>
+            <p>High Score: {highScore}</p>
+            </div>
         </section>
     );
 }
